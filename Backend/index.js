d610d72400ece0f6c9677cd0fc5c9f793edba788
Andrew <amccan5@uwo.ca>
2017-09-23 04:39:11 -0400

    updates to socket and multi-room

--- Backend/index.js
+++ Backend/index.js
@@ -12,14 +12,40 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    console.log('message: ' + msg);
+    var translated = translate.translateMessage(msg, 'en', 'zh');
   });
 });
 
-io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
+// Current users in chat room.
+var usernames = {};
+
+io.sockets.on('connection', function (socket) {
+
+	// Call to room management, place user in new room or wait.
+	socket.on('adduser', function(userID, fromLang, toLang)  {
+		socket.userID = userID;
+		socket.roomID = roomID;
+		usernames[userID] = userID;
+		socket.join(roomID);
+		socket.broadcast.to(roomID).emit('updatechat', 'SERVER', 'Someone has joined you. Say hello!');
+		socket.emit('updaterooms', rooms, 'room1');
+	});
+
+	// User sends message, call translation, and update chatroom.
+	socket.on('sendchat', function(msg, fromLang, toLang) {
+		var translated = translate.translateMessage(msg, fromLang, toLang);
+
 		console.log('message: ' + msg);
-		var translated = translate.translateMessage(msg, 'en', 'zh');
-		//translate.translateMessage('yo what up', 'EN', 'IT');
+		io.sockets.in(socket.room).emit('updatechat', socket.userID, translated);
+		io.sockets.in(socket.room).emit('updatechat', socket.userID, msg);
+	});
+
+	// User disconnects from chat.
+	socket.on('disconnect', function() {
+		delete usernames[socket.userID];
+		io.sockets.emit('updateusers', usernames);
+		socket.leave(socket.room);
 	});
 });
 
